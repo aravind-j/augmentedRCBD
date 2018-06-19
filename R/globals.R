6e056d62611530ab5bba859d5d6194a32d9486bd
J. Aravind <aravindjayaramanagri@gmail.com>
2018-06-20 00:23:58 +0530

    Added global variables

---
+++ R/globals.R
@@ -0,0 +1,27 @@
+### This file is part of 'PGRdup' package for R.
+
+### Copyright (C) 2014, ICAR-NBPGR.
+#
+# PGRdup is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# PGRdup is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+#  A copy of the GNU General Public License is available at
+#  https://www.r-project.org/Licenses/
+
+
+
+if (getRversion() >= "2.15.1")  {
+  utils::globalVariables(
+    c(
+      "xmax", "xmin", "ymax", "ymin", "Means", "lower", "upper", "Category",
+      "Trait", "Treatment", "sl", "value", "variable"
+    )
+  )
+}
