cddee1d742d97a0e397cf4ec6ade53d65562d338
J. Aravind <aravindjayaramanagri@gmail.com>
2018-06-20 00:23:41 +0530

    Added launch message

---
+++ R/xtra.R
@@ -0,0 +1,19 @@
+
+wlcm <- paste0("\n",
+               "--------------------------------------------------------------------------------\n",
+               "Welcome to augmentedRCBD version ", utils::packageDescription("augmentedRCBD")$Version, "\n",
+               "\n", "\n",
+               "# To know whats new in this version type:", "\n",
+               "  news(package='augmentedRCBD')", "\n", "  for the NEWS file.", "\n",
+               "\n",
+               "# To cite the methods in the package type:", "\n",
+               "  citation(package='augmentedRCBD')","\n",
+               "\n",
+               "# To suppress this message use:", "\n",
+               "  suppressPackageStartupMessages(library(augmentedRCBD))", "\n",
+               "--------------------------------------------------------------------------------\n")
+
+.onAttach <- function(lib, pkg,...){
+  packageStartupMessage(wlcm)
+
+}
