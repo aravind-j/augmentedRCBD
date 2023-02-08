ca3a100ef7c1b4c83948b7f8a922a7eabfce22c5
J. Aravind <aravindjayaramanagri@gmail.com>
2023-02-08 20:34:44 +0530

    Update copyright year

--- R/globals.R
+++ R/globals.R
@@ -1,6 +1,6 @@
 ### This file is part of 'PGRdup' package for R.
 
-### Copyright (C) 2015-2022, ICAR-NBPGR.
+### Copyright (C) 2015-2023, ICAR-NBPGR.
 #
 # PGRdup is free software: you can redistribute it and/or modify it
 # under the terms of the GNU General Public License as published by
