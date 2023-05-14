80487ca4ff165f3cdf21839c3c08c570a8eace24
J. Aravind <aravindjayaramanagri@gmail.com>
2023-05-14 15:44:19 +0530

    Replace PGRdup

--- R/globals.R
+++ R/globals.R
@@ -1,13 +1,13 @@
-### This file is part of 'PGRdup' package for R.
+### This file is part of 'augmentedRCBD' package for R.
 
 ### Copyright (C) 2015-2023, ICAR-NBPGR.
 #
-# PGRdup is free software: you can redistribute it and/or modify it
+# augmentedRCBD is free software: you can redistribute it and/or modify it
 # under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 2 of the License, or
 # (at your option) any later version.
 #
-# PGRdup is distributed in the hope that it will be useful, but
+# augmentedRCBD is distributed in the hope that it will be useful, but
 # WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
